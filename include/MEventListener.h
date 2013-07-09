34e786e3b583d3c689b7f4bb75505a302617638e
Philipp Geyer <philipp@geyer.co.uk>
2013-07-09 17:36:59 +0300

    updated MEvent to not complain about linking (removed MEventListener constructor/destructor and moved them into macro)

--- include/MEventListener.h
+++ include/MEventListener.h
@@ -1,14 +1,29 @@
 #ifndef __M_EVENT_LISTENER_H__
 #define __M_EVENT_LISTENER_H__
 
+#include "MGameEvent.h"
+
 class MEventListener
 {
 public:
     virtual void onEvent(const char* event) = 0;
-
-public:
-    MEventListener();
-    ~MEventListener();
 };
+
+#define MEventListenerDeclare(x)        \
+x()                                     \
+{                                       \
+    MGameEvent* game = NULL;            \
+    MGameEventGet(game);                \
+    if(game)                            \
+        game->registerListener(this);   \
+}                                       \
+~x()                                    \
+{                                       \
+    MGameEvent* game = NULL;            \
+    MGameEventGet(game);                \
+    if(game)                            \
+        game->unregisterListener(this); \
+}
+
 
 #endif/*__M_EVENT_LISTENER_H__*/
